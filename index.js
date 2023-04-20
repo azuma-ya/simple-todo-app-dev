09b4f83fb3028f6d0bf4ed5c9eea5ac7d62a4639
azuma-ya <kazu.bluemountain@gmail.com>
2023-04-20 23:49:28 +0900

    3rd commit

--- index.js
+++ index.js
@@ -1,41 +1,65 @@
 const addTaskButtonDom = document.getElementById("addTaskButton");
 const taskInputDom = document.getElementById("taskInput");
 const taskListDom = document.getElementById("taskList");
-const taskCheckboxDoms = document.querySelectorAll("task-checkbox");
 
-let todoList = [];
+let taskList = [];
 
-const crateTaskItem = (text) =>
-	`<li>
-		<input type="checkbox" class="task-checkbox"/>
-		${text}
+const crateTaskItem = (task) =>
+	`<li class="task">
+		<input type="checkbox" class="task-checkbox" data-id="${task.id}"/>
+		<span>${task.title}</span>
 		<i class='bx bx-trash'></i>
 	</li>`;
 
 addTaskButtonDom.addEventListener("click", (e) => {
 	e.preventDefault();
+	if (taskInputDom.value === "") return;
 
-	localStorage.setItem(
-		"items",
-		JSON.stringify([...todoList, taskInputDom.value])
-	);
-	const item = crateTaskItem(taskInputDom.value);
+	const task = {
+		id: ~~(Math.random() * 1000000000),
+		title: taskInputDom.value,
+		checked: false,
+	};
+	taskList.push(task);
+	localStorage.setItem("items", JSON.stringify(taskList));
+	const item = crateTaskItem(task);
 	taskListDom.innerHTML += item;
 	taskInputDom.value = "";
 });
 
-taskCheckboxDoms.forEach((taskInputDom) => {
-	taskInputDom.addEventListener("change", () => {
-		console.log("click");
-	});
+taskListDom.addEventListener("click", (e) => {
+	// console.log(e.target);
+	if (e.target.classList.contains("task-checkbox")) {
+		const id = Number(e.target.dataset.id);
+		taskList.forEach((task) => {
+			if (task.id === id) {
+				task.checked = !task.checked;
+				e.target.checked = task.checked;
+				if (task.checked) {
+					e.target.parentElement.style.textDecoration = "line-through";
+				} else {
+					e.target.parentElement.style.textDecoration = "none";
+				}
+			}
+		});
+		localStorage.setItem("items", JSON.stringify(taskList));
+
+		// taskListDom.innerHTML = "";
+		// taskList.forEach((task) => {
+		// 	const item = crateTaskItem(task);
+		// 	taskListDom.innerHTML += item;
+		// });
+	}
 });
 
 window.addEventListener("load", () => {
-	todoList = JSON.parse(localStorage.getItem("items"));
-	if (todoList) {
-		todoList.forEach((itemText) => {
-			const item = crateTaskItem(itemText);
+	taskList = JSON.parse(localStorage.getItem("items"));
+	if (taskList) {
+		taskList.forEach((task) => {
+			const item = crateTaskItem(task);
 			taskListDom.innerHTML += item;
 		});
+	} else {
+		taskList = [];
 	}
 });
